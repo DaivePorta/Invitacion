167cc63b040fa8eab9bac1b79ffff679cf1d9226
Daive <daive.u17@gmail.com>
2025-02-05 17:56:01 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+$( document ).ready(function() {
+    
+    var sobre = $('#envelope');
+    var btn_abrir = $("#open");
+    var btn_cerrar = $("#reset");
+    
+    var btn_1 = document.getElementById('open');
+    var btn_2 = document.getElementById('reset');
+
+    sobre.click( function() {
+        abrir();
+    });
+
+    btn_abrir.click( function() {
+        abrir();
+    });
+
+    btn_cerrar.click( function() {
+        cerrar();
+    });
+
+    function abrir() {
+        sobre.addClass("open")
+           .removeClass("close");
+           //$("#open").fadeOut();
+           //$("#reset").fadeIn();   
+           btn_1.style.display = 'none';
+           btn_2.style.display = 'inline';        
+    }
+    
+    function cerrar() {
+        sobre.addClass("close")
+           .removeClass("open");
+           //$("#reset").fadeOut();
+           //$("#open").fadeIn();      
+           btn_2.style.display = 'none';
+           btn_1.style.display = 'inline';     
+    }   
+});
